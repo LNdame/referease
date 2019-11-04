2b99ce898d08bb8fd912d6deca4fb0feea6860f7
Loic <loic.ndame@superbalist.com>
2019-11-04 20:24:43 +0200

    citation

--- lib/uipage/widgets/source_card.dart
+++ lib/uipage/widgets/source_card.dart
@@ -2,11 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:referease/model/source.dart';
 import 'package:referease/model/source_model.dart';
+import 'package:referease/uipage/pages/helpers/source_open_reference_view.dart';
 
 class SourceCard extends StatelessWidget {
   final SourceModel source;
 
   SourceCard({this.source});
+
 
   @override
   Widget build(BuildContext context) {
@@ -75,8 +77,19 @@ class SourceCard extends StatelessWidget {
                       width: 18.0,
                     ),
                     PopupMenuButton<String>(
-                      onSelected: (option) {
-                        print(option);
+                      onSelected: (option){
+                        Source aSource = getBareSource(source);
+                        if(option==Constants.havard){
+                          print(" ${aSource.toHarvard()}");
+                          openReferenceView(context, aSource.toHarvard(), "Havard");
+                          //  Share.share(" ${abook.toHarvard()}");
+                        }else if(option == Constants.apa){
+                          openReferenceView(context, aSource.toApa(), "APA");
+                          print(" ${aSource.toApa()}");
+                          //  Share.share(" ${abook.toApa()}"); //it works on actual device
+                        }
+
+
                       },
                       itemBuilder: (BuildContext context) {
                         return Constants.options.map((String option) {
@@ -116,6 +129,73 @@ class SourceCard extends StatelessWidget {
         break;
     }
   }
+
+  Source getBareSource (SourceModel sourceModel){
+    switch (sourceModel.source_type_id) {
+      case 1:
+        return new Book(authors: sourceModel.authors,
+            city: sourceModel.city,
+            title: sourceModel.title,
+            doi: sourceModel.doi,
+            edition: sourceModel.edition,
+            editors: sourceModel.editors,
+            isbn: sourceModel.isbn,
+            publisher: sourceModel.publisher,
+            year: sourceModel.year,
+            pages: sourceModel.pages);
+        break;
+      case 2:
+        return new ConferenceProceeding(title: sourceModel.title,
+            authors: sourceModel.authors,
+            proceTitle: sourceModel.proceeding_title,
+            year: sourceModel.year,
+            pages: sourceModel.pages,
+            city: sourceModel.city,
+            doi: sourceModel.doi,
+            editors: sourceModel.editors,
+            url: sourceModel.url,
+            publisher: sourceModel.publisher);
+        break;
+      case 3:
+        return new JournalArticle(title: sourceModel.title,
+            authors: sourceModel.authors,
+            journal: sourceModel.journal,
+            year: sourceModel.year,
+            pages: sourceModel.pages,
+            doi: sourceModel.doi,
+            volume: sourceModel.volume,
+            issue: sourceModel.issue,
+            url: sourceModel.url,
+            publisher: sourceModel.publisher);
+        break;
+      case 4:
+        return new Web(authors: sourceModel.authors,
+            title: sourceModel.title,
+            publication: sourceModel.publication,
+            year: sourceModel.year,
+            dateAccessed: sourceModel.date_accessed,
+            url: sourceModel.url,
+            doi: sourceModel.doi,
+            pages: sourceModel.pages);
+        break;
+      default:
+        return new Book(authors: sourceModel.authors,
+            city: sourceModel.city,
+            title: sourceModel.title,
+            doi: sourceModel.doi,
+            edition: sourceModel.edition,
+            editors: sourceModel.editors,
+            isbn: sourceModel.isbn,
+            publisher: sourceModel.publisher,
+            year: sourceModel.year,
+            pages: sourceModel.pages);
+        break;
+    }
+
+  }
+
+
+
 }
 
 class Constants {
@@ -124,3 +204,36 @@ class Constants {
 
   static const List<String> options = <String>[havard, apa];
 }
+
+/*
+if(type==TypeConstant.book){
+
+aSource = new Book(authors: authors, city: city, title: title, doi:doi,edition: edition, editors: editor,
+isbn: isbn, publisher: publisher,type: type,uid: uid,year: year,pages: pages, docID: docID);
+sType = prefix0.SourceTypeName.book;
+icon = Icons.book;
+
+}else if(type==TypeConstant.conference_proceeding){
+
+aSource = new ConferenceProceeding( title: title, authors: authors, proceTitle: proceTitle, year: year,
+pages: pages, city: city,
+doi:doi, editors: editor, url: url, publisher: publisher,type: type,uid: uid,docID: docID);
+
+sType = prefix0.SourceTypeName.conference_proceeding;
+icon = Icons.description;
+
+}else if(type==TypeConstant.journal_article){
+
+aSource = new JournalArticle(title: title, authors: authors, journal:journal, year: year,pages: pages ,
+doi:doi,volume: volume, issue: issue, url: url, publisher: publisher,type: type,docID: docID);
+sType = prefix0.SourceTypeName.journal_article;
+icon = Icons.import_contacts;
+
+}else if(type==TypeConstant.web){
+
+aSource = new Web(authors: authors,  title: title, publication:publication,year: year,dateAccessed: dateAccessed,
+url: url,uid: uid,  doi:doi, pages: pages,type: type,docID: docID);
+sType = prefix0.SourceTypeName.web;
+icon = Icons.public;
+
+} */
